7fefbfca9c4ad33184c1b4ae3d706a21279317a0
Caleb Stanford <caleb.pirsquared@gmail.com>
2021-05-04 16:25:25 -0400

    minor demo update

--- demos/src/bin/structopt.rs
+++ demos/src/bin/structopt.rs
@@ -3,17 +3,19 @@ use structopt::StructOpt;
 #[derive(Debug, StructOpt)]
 #[structopt(name = "CIS198 CLI", about = "A simple CLI example in Rust.")]
 struct CLI {
-    #[structopt(short, long)]
-    name: String,
     #[structopt(short, long, default_value = "Hello")]
     greeting: String,
+    #[structopt(short, long, required = true)]
+    names: Vec<String>,
     #[structopt(short, long)]
     debug: bool,
 }
 
 fn main() {
     let args = CLI::from_args();
-    println!("{}, {}!", args.greeting, args.name);
+    for name in &args.names {
+        println!("{}, {}!", args.greeting, name);
+    }
     if args.debug {
         dbg!(args);
     }
